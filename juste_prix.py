fc01b59b224d821c8e02cb270efd4c343d05fb88
LaryGuignet <88318918+LaryGuignet@users.noreply.github.com>
2021-08-05 17:33:11 +0200

    Ajout de la gestion d'erreur pour l'entrée

--- juste_prix.py
+++ juste_prix.py
@@ -1,11 +1,5 @@
 from random import *
 
-nombre_min = int(input("\nDonner une borne inférieur : "))
-nombre_max = int(input("Donner une borne supérieur : "))
-nombre = randint(nombre_min, nombre_max)
-print("\nTrouvez un nombre entre {} et {} inclus.".format(nombre_min, nombre_max))
-hypothese = int(input("Donnez votre nombre : "))
-
 def juste_prix (prix, hypothese, nombre_min, nombre_max) :
     if nombre != hypothese :
         while prix != hypothese :
@@ -15,7 +9,16 @@ def juste_prix (prix, hypothese, nombre_min, nombre_max) :
             else :
                 print("Moins")
                 hypothese = int(input("Donnez un nouveau nombre : "))
-    
     return print("Vous avez trouvez le bon nombre !\n")
 
-juste_prix(nombre, hypothese, nombre_min, nombre_max)
+try :
+    nombre_min = int(input("\nDonner une borne inférieur : "))
+    nombre_max = int(input("Donner une borne supérieur : "))
+    nombre = randint(nombre_min, nombre_max)
+    print("\nTrouvez un nombre entre {} et {} inclus.".format(nombre_min, nombre_max))
+    hypothese = int(input("Donnez votre nombre : "))
+    juste_prix(nombre, hypothese, nombre_min, nombre_max)
+except ValueError : 
+    print("Vous n'avez pas entré un nombre.")
+finally :
+    print("Fin du programme.")
