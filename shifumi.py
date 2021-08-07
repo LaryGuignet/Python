8c92d5b138fd2dbfd3b61c6b7702dc651863f570
LaryGuignet <88318918+LaryGuignet@users.noreply.github.com>
2021-08-07 20:05:54 +0200

    Ajout de la gestion d'erreur
    - vérification que le nombre de manche gagnante entré est valide (int)
    - vérification de l'entrée d'un coup réglementaire

--- shifumi.py
+++ shifumi.py
@@ -20,7 +20,7 @@ def coup_humain(choix) :
     elif choix == "ciseaux" :
         nombre = 3
     else :
-        print("Problème de conversion 1")
+        nombre = 4
     return nombre
 
 def gagnant(humain, ordi) :
@@ -38,21 +38,29 @@ def compteur(points_gagnant) :
 
 
 print("\nLancement d'une partie de shifumi ...")
-nombre_manches = int(input("Combien de manche pour gagner : "))
-humain_points = 0
-ordi_points = 0
-while humain_points != nombre_manches and ordi_points != nombre_manches :
-    nombre_ordi = randint(1, 3)
-    choix = input("Quel coup voulez vous faire ?\t")
-    # demande du choix
-    print ("L'ordi fait {}.".format(coup_ordi(nombre_ordi)))
-    nombre_humain = coup_humain(choix)
-    #  conversion du choix en nombre
-    resultat = gagnant(nombre_humain, nombre_ordi)
-    if resultat == "humain" :
-        humain_points = compteur(humain_points)
-    elif resultat == "ordi" :
-        ordi_points = compteur(ordi_points)
-    # comptage des points
-    print ("Humain = {} | Ordi = {}\n".format(humain_points, ordi_points))
+try :
+    nombre_manches = int(input("Combien de manche pour gagner : "))
+except ValueError:
+    print("Vous n'avez pas entré un nombre.")
+else :
+    humain_points = 0
+    ordi_points = 0
+    while humain_points != nombre_manches and ordi_points != nombre_manches :
+        nombre_ordi = randint(1, 3)
+        choix = input("Quel coup voulez vous faire ( pierre | feuille | ciseaux ) ?\t")
+        # demande du choix
+        # exception sur l'entrée != 'pierre', 'feuille', ou 'ciseaux' (nombre = 4)
+        nombre_humain = coup_humain(choix)
+        #  conversion du choix en nombre
+        if nombre_humain == 4 :
+            print("Coup non réglementaire !")
+        else :
+            print ("L'ordi fait {}.".format(coup_ordi(nombre_ordi)))
+            resultat = gagnant(nombre_humain, nombre_ordi)
+            if resultat == "humain" :
+                humain_points = compteur(humain_points)
+            elif resultat == "ordi" :
+                ordi_points = compteur(ordi_points)
+                # comptage des points
+            print ("Humain = {} | Ordi = {}\n".format(humain_points, ordi_points))
 print("Fin de la partie ...")
